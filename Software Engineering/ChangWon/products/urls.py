88d37a5cf6d7f0df3591fae93a7a531a4ad61708
seeun0603 <48176534+seeun0603@users.noreply.github.com>
2020-02-15 15:50:44 +0900

    Revert "SE Project"
    This reverts commit c522fdf2382e63a78c3507265c6d706e2c07300e.

--- Software Engineering/ChangWon/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name="productsList"),
-    path('order/', views.order, name="order"),
-    path('<int:pk>/', views.product_view, name='product_view'),
-]
